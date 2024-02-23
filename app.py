3a45d217587acab95dc82e7fe09bbe945094758f
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2024-02-23 16:57:31 +0100

    [Logging] Finished Task 1 : Add a logging to your application that measures the duration of the method associated with the endpoint /search. This will measure the latency of each call. The logging should log metrics into a file.

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, render_template
 import requests
+import logging
+import time
 
 app = Flask(__name__)
+
+app.logger.setLevel(logging.INFO)
+handler = logging.FileHandler('app.log')
+app.logger.addHandler(handler)
 
 
 @app.route("/")
@@ -12,7 +18,8 @@ def home():
 
 @app.route("/search", methods=["POST"])
 def search():
-
+    start_time = time.time()
+    print(start_time);  
     # Get the search query
     query = request.form["q"]
 
@@ -26,14 +33,16 @@ def search():
     if location:
         coordinate = [location[0]["lat"], location[0]["lon"]]
 
-        time = requests.get(
+        response = requests.get(
             "https://timeapi.io/api/Time/current/coordinate",
             {"latitude": coordinate[0], "longitude": coordinate[1]},
         )
-
-        return render_template("success.html", location=location[0], time=time.json())
+        duration = time.time() - start_time
+        app.logger.info('Search took %s seconds', duration)
+        return render_template("success.html", location=location[0], time=response.json())
 
     # If a location is NOT found, return the error page
     else:
-
+        duration = time.time() - start_time
+        app.logger.info('Search took %s seconds', duration)
         return render_template("fail.html")
